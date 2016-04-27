86a8fb482383e07ee4933122e145ad42627b0f9c
Deniz Dogan <deniz@dogan.se>
2016-04-27 18:49:05 +0200

    Format source code according to standard

--- src/index.js
+++ src/index.js
@@ -1,149 +1,148 @@
-import fs from 'fs';
-import request from 'request-promise';
-import urljoin from 'url-join';
+import fs from 'fs'
+import request from 'request-promise'
+import urljoin from 'url-join'
 
-const API_BASE_URL = 'https://api.streamable.com';
+const API_BASE_URL = 'https://api.streamable.com'
 
 export class Streamable {
 
-	/**
-	 * Upload a video stream to Streamable
-	 * @param {string} stream the stream of the file
-	 * @param {string} [title=''] title of the video
-	 * @return {Promise} A promise to return the response
-	 */
-	uploadStream(stream, title = '') {
-		const data = {
-			file: stream,
-			title
-		};
-		return this._post('/upload', data);
-	}
+  /**
+   * Upload a video stream to Streamable
+   * @param {string} stream the stream of the file
+   * @param {string} [title=''] title of the video
+   * @return {Promise} A promise to return the response
+   */
+  uploadStream (stream, title = '') {
+    const data = {
+      file: stream,
+    title}
+    return this._post('/upload', data)
+  }
 
-	/**
-	 * Upload a video to Streamable
-	 * @param {string} filePath the path to the file
-	 * @param {string} [title=''] title of the video
-	 * @return {Promise} A promise to return the response
-	 */
-	uploadVideo(filePath, title = '') {
-		const stream = fs.createReadStream(filePath);
-		return this.uploadStream(stream, title);
-	}
+  /**
+   * Upload a video to Streamable
+   * @param {string} filePath the path to the file
+   * @param {string} [title=''] title of the video
+   * @return {Promise} A promise to return the response
+   */
+  uploadVideo (filePath, title = '') {
+    const stream = fs.createReadStream(filePath)
+    return this.uploadStream(stream, title)
+  }
 
-	/**
-	 * Import a video to Streamable
-	 * @param {string} url the URL to the video
-	 * @param {string} [title] title of the video
-	 * @return {Promise} A promise to return the response
-	 */
-	importVideo(url, title = '') {
-		const qs = {url, title};
-		return this._get('/import', qs);
-	}
+  /**
+   * Import a video to Streamable
+   * @param {string} url the URL to the video
+   * @param {string} [title] title of the video
+   * @return {Promise} A promise to return the response
+   */
+  importVideo (url, title = '') {
+    const qs = {url, title}
+    return this._get('/import', qs)
+  }
 
-	/**
-	 * Retrieve a video from Streamable
-	 * @param {string} shortcode the shortcode of the video
-	 * @return {Promise} A promise to return the response
-	 */
-	retrieveVideo(shortcode) {
-		return this._get(`/videos/${shortcode}`);
-	}
+  /**
+   * Retrieve a video from Streamable
+   * @param {string} shortcode the shortcode of the video
+   * @return {Promise} A promise to return the response
+   */
+  retrieveVideo (shortcode) {
+    return this._get(`/videos/${shortcode}`)
+  }
 
-	/**
-	 * Retrieve a user on Streamable
-	 * @param {string} username the name of the user
-	 * @return {Promise} A promise to return the response
-	 */
-	retrieveUser(username) {
-		return this._get(`/users/${username}`);
-	}
+  /**
+   * Retrieve a user on Streamable
+   * @param {string} username the name of the user
+   * @return {Promise} A promise to return the response
+   */
+  retrieveUser (username) {
+    return this._get(`/users/${username}`)
+  }
 
-	/**
-	 * Make an HTTP request to Streamable
-	 * @param {string} method the HTTP method to use
-	 * @param {string} path the path
-	 * @param {object} [options={}] additional options
-	 * @return {Promise} A promise to return the response
-	 */
-	_request(method, path, options = {}) {
-		const url = urljoin(API_BASE_URL, path);
-		options = this._prepareOptions(method, path, options);
-		return request(url, options);
-	}
+  /**
+   * Make an HTTP request to Streamable
+   * @param {string} method the HTTP method to use
+   * @param {string} path the path
+   * @param {object} [options={}] additional options
+   * @return {Promise} A promise to return the response
+   */
+  _request (method, path, options = {}) {
+    const url = urljoin(API_BASE_URL, path)
+    options = this._prepareOptions(method, path, options)
+    return request(url, options)
+  }
 
-	/**
-	 * Make an HTTP GET request to Streamable
-	 * @param {string} path the path
-	 * @param {object} [qs={}] the querystring
-	 * @return {Promise} A promise to return the response
-	 */
-	_get(path, qs = {}) {
-		return this._request('GET', path, {qs});
-	}
+  /**
+   * Make an HTTP GET request to Streamable
+   * @param {string} path the path
+   * @param {object} [qs={}] the querystring
+   * @return {Promise} A promise to return the response
+   */
+  _get (path, qs = {}) {
+    return this._request('GET', path, {qs})
+  }
 
-	/**
-	 * Make an HTTP POST request as "multipart/form" to Streamable
-	 * @param {string} path the path
-	 * @param {object} [data={}] the data
-	 * @return {Promise} A promise to return the response
-	 */
-	_post(path, data = {}) {
-		return this._request('POST', path, {
-			formData: data
-		});
-	}
+  /**
+   * Make an HTTP POST request as "multipart/form" to Streamable
+   * @param {string} path the path
+   * @param {object} [data={}] the data
+   * @return {Promise} A promise to return the response
+   */
+  _post (path, data = {}) {
+    return this._request('POST', path, {
+      formData: data
+    })
+  }
 
-	/**
-	 * Prepare an object with request options
-	 * @param {string} method the HTTP method to use
-	 * @param {string} path the path to request
-	 * @param {object} [options={}] additional request options
-	 * @return {object} Request options object
-	 */
-	_prepareOptions(method, path, options = {}) {
-		return Object.assign({}, options, {
-			method,
-			json: true
-		});
-	}
+  /**
+   * Prepare an object with request options
+   * @param {string} method the HTTP method to use
+   * @param {string} path the path to request
+   * @param {object} [options={}] additional request options
+   * @return {object} Request options object
+   */
+  _prepareOptions (method, path, options = {}) {
+    return Object.assign({}, options, {
+      method,
+      json: true
+    })
+  }
 }
 
 export class AuthStreamable extends Streamable {
 
-	/**
-	 * @constructor
-	 * @param {string} username The username to log in with
-	 * @param {string} password The password to log in with
-	 */
-	constructor(username, password) {
-		super();
-		this.username = username;
-		this.password = password;
-	}
+  /**
+   * @constructor
+   * @param {string} username The username to log in with
+   * @param {string} password The password to log in with
+   */
+  constructor (username, password) {
+    super()
+    this.username = username
+    this.password = password
+  }
 
-	/**
-	 * Retrieve information about the currently logged-in user
-	 * @return {Promise} A promise to return the response
-	 */
-	retrieveMe() {
-		return this._get('/me');
-	}
+  /**
+   * Retrieve information about the currently logged-in user
+   * @return {Promise} A promise to return the response
+   */
+  retrieveMe () {
+    return this._get('/me')
+  }
 
-	/**
-	 * Prepare an object with request options, including authentication
-	 * @param {string} method The HTTP method
-	 * @param {string} path The HTTP path
-	 * @return {object} Request options object
-	 */
-	_prepareOptions(method, path, options = {}) {
-		options = super._prepareOptions(method, path, options);
-		return Object.assign({}, options, {
-			auth: {
-				username: this.username,
-				password: this.password
-			}
-		});
-	}
+  /**
+   * Prepare an object with request options, including authentication
+   * @param {string} method The HTTP method
+   * @param {string} path The HTTP path
+   * @return {object} Request options object
+   */
+  _prepareOptions (method, path, options = {}) {
+    options = super._prepareOptions(method, path, options)
+    return Object.assign({}, options, {
+      auth: {
+        username: this.username,
+        password: this.password
+      }
+    })
+  }
 }
